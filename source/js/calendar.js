cf408909ad1d77f6623f54c20c992efe0756cad8
licc <961150665@qq.com>
2020-12-13 23:57:42 +0800

    new: 贡献墙1.0版本（废弃）

--- source/js/calendar.js
+++ source/js/calendar.js
@@ -1,1 +1,6 @@
 GitHubCalendar(".calendar", "LeeDebug", { responsive: true });
+// GitHubCalendar(".calendar", "LeeDebug", {
+//   proxy (username) {
+//     return fetch(`https://githubapi.ryanchristian.dev/user/${username}`)
+//   }
+// }).then(r => r.text())
