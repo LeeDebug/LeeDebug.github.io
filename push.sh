9bb586e4655b065ba51a12778a530457fcfb6e7a
licc <961150665@qq.com>
2020-12-26 23:46:57 +0800

    hexo new post git仓库推送脚本（本地简易)

--- push.sh
+++ push.sh
@@ -8,3 +8,14 @@ git commit -m "$MSG"
 
 echo → 将代码推送至三端git仓库
 git push -u all hexo
+
+# 复制文件内容
+# cp -r -f /Users/qducc/Documents/0_blog_posts/ /Users/qducc/Qducc/blog/source/_posts
+
+# 删除 9_blog 文件夹下的所有内容
+# rm -rf /Users/qducc/Documents/0_blog_posts/*
+
+echo → Hexo自动构建及部署
+npm run clean
+npm run build
+npm run deploy
--- release.sh
+++ release.sh
@@ -1,15 +1,4 @@
-# 复制文件内容
-# cp -r -f /Users/qducc/Documents/0_blog_posts/ /Users/qducc/Qducc/blog/source/_posts
-
-# 删除 9_blog 文件夹下的所有内容
-# rm -rf /Users/qducc/Documents/0_blog_posts/*
-
-# 自动推送代码
-# git add .
-# git commit -m '自动化部署的注释'
-# git push origin master
-
-# 自动化构建及部署
+echo → Hexo自动构建及部署
 npm run clean
 npm run build
 npm run deploy
